dff0df534437773b44cd98d756e21fb34b2c1eb9
Junaid Ali Hakro <junaid.hakro@systemsltd.com>
2023-09-13 17:41:23 +0500

    [ADD] Home, Not found & Login Pages component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/ui/navbar/navbar.component';
-
-const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NotFoundComponent } from './views/not-found/not-found.component';
+import { LoginComponent } from './views/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, NotFoundComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
